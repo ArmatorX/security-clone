f5bfa1466b6b071bad3d1b9bdc92a189c8f51a38
Federico Alejo Marquez <17768182+fefu-marquez@users.noreply.github.com>
2023-10-09 16:58:03 -0300

    Test: Add tests for Goal

--- Assets/Code/Scripts/Interactive/Goal.cs
+++ Assets/Code/Scripts/Interactive/Goal.cs
@@ -2,16 +2,9 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    private Controller _gameController;
-
-    void Awake()
-    {
-        _gameController = AlwaysOnScene.GameController;
-    }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-            _gameController.Win();
+            AlwaysOnScene.GameController.Win();
     }
 }
---
+++ Assets/Code/Tests/Interactive/Goal.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using System.Collections;
+
+[TestFixture]
+public class GoalPrefab
+{
+    private GameObject goalPrefab;
+    private GameObject spyPrefab;
+    private GameObject wallPrefab;
+
+    public GameObject goal;
+    public GameObject entity;
+    public GameObject gameController;
+    public DummyController dummyController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        goalPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Level/Prefabs/LevelElements/Interactive/Goal.prefab");
+        spyPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Level/Prefabs/Spy.prefab");
+        wallPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Level/Prefabs/LevelElements/Wall.prefab");
+
+        goal = GameObject.Instantiate(goalPrefab, Vector3.zero, Quaternion.identity);
+
+        gameController = new GameObject("GameController");
+        dummyController = gameController.AddComponent<DummyController>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        GameObject.DestroyImmediate(goal);
+        GameObject.DestroyImmediate(entity);
+        GameObject.DestroyImmediate(gameController);
+    }
+
+    [UnityTest]
+    public IEnumerator OnTriggerEnter2D_EntityPressedSwitch_ShouldNotCallWin()
+    {
+        entity = GameObject.Instantiate(wallPrefab, Vector3.zero, Quaternion.identity);
+
+        yield return new WaitForFixedUpdate();
+
+        Assert.False(dummyController.HasCalledWin);
+    }
+
+    [UnityTest]
+    public IEnumerator OnTriggerEnter2D_PlayerPressedSwitch_ShouldCallWin()
+    {
+        entity = GameObject.Instantiate(spyPrefab, Vector3.zero, Quaternion.identity);
+
+        yield return new WaitForFixedUpdate();
+
+        Assert.True(dummyController.HasCalledWin);
+    }
+}
